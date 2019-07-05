e88872f68a54f82a594b6904002f56d20a50d433
Fitriyani Kiat <49054559+Fitrikiat@users.noreply.github.com>
2019-07-05 16:55:15 +0100

    Create detailfilmdua.php

---
+++ detailfilmdua.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>detail</title>
+</head>
+<body>
+<table width='100%'><br/> //ukuran dari suatu film
+          <tr><td width='20%'><b>Judul Film</b></td> <td>:</td><td style='color:red'> The Hunger Games</td></tr> //menampilkan judul film
+		  
+		  <tr><td><b>Tanggal Tayang</b></td> <td>:</td>  	 <td style='color:blue'> 12 Maret 2012</td></tr>  //tanggal tayang film
+		  <tr><td><b>Harga Tiket</b></td> <td>:</td>  	 <td style='color:blue'> Rp 30.000</td></tr>  //harga tiket film
+		  		  <tr><td valign=top><b>Produser</b></td> <td>:</td>  	 <td style='color:red'>Nina jacobson,Jon Kilik</td></tr>  //memasukkan warna pada layout/tampilan web
+				  <tr><td valign=top><b>Produksi</b></td> <td>:</td>  	 <td style='color:red'> Submit Entertainment</td></tr>  
+				  <tr><td><b>Durasi</b></td> <td>:</td>  	 <td style='color:red'> 142 Menit</td></tr>  //durasi berapa lama tayangnya suatu film
+		  <tr><td valign=top><b>Sinopsis</b></td>  <td>:</td>  	
+		  <td>The Hunger Games menceritakan tentang seorang gadis berusia 16 tahun bernama Katniss Everdeen (Jennifer Lawrence) yang tinggal di sebuah tempat bernama Distrik 12, di mana mayoritas penduduknya bekerja sebagai penambang. Distrik 12 adalah sebuah distrik terakhir, salah satu dari 12 distrik di negara yang disebut Panem. Panem sendiri merupakan sebuah negara dimana dulunya Amerika Utara pernah berada, yang setelah bencana besar di Bumi menjadi satu-satunya wilayah yang selamat. Karena pemberontakan gagal terhadap pemerintahan Panem 75 tahun sebelumnya, pemerintahan Panem lantas melakukan sebuah kompetisi maut yang diadakan di ibukota negara yang bernama Capitol.<br/><br/></td></tr> //menjelaskan sinopsis dari suatu film
+	  </table>
+	  <input style='width:100px;' type=button value='Pesan Tiket' onclick="window.location.href='kursi1.php';"> //menginputkan kursi yang nanti akan kita duduk untuk nonton
+</body>
+</html>
