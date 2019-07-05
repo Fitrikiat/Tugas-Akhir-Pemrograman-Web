8c5d374b5477ff005d5193ec67a4d8c62d7145b9
Fitriyani Kiat <49054559+Fitrikiat@users.noreply.github.com>
2019-07-05 16:59:37 +0100

    Create detailfilmtiga.php

---
+++ detailfilmtiga.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>detail</title>
+</head>
+<body>
+<table width='100%'><br/> //ukuran tabel pada layout web
+          <tr><td width='20%'><b>Judul Film</b></td> <td>:</td><td style='color:red'> Rudi Habibie</td></tr> //menampilkan judul film
+		 
+		  <tr><td><b>Tanggal Tayang</b></td> <td>:</td>  	             <td style='color:blue'> 30 juni 2016</td></tr>  //tanggal tayang dan tampilan warna pada web
+		  <tr><td><b>Harga Tiket</b></td> <td>:</td>  	                 <td style='color:blue'> Rp 30.000</td></tr>  //harga tiket film
+		  <tr><td valign=top><b>Sutradara</b></td> <td>:</td>  	         <td style='color:red'> Hanung Bramantyo</td></tr> 
+
+	      <tr><td valign=top><b>Produser</b></td> <td>:</td>  	 <td style='color:red'> Manoj Punjabi</td></tr>   
+		  <tr><td><b>Durasi</b></td> <td>:</td>  	             <td style='color:red'> 103 Menit</td></tr>  
+		  <tr><td valign=top><b>Sinopsis</b></td>  <td>:</td>  			 <td> masa muda dari seorang visioner bernama Rudy (panggilan kecil B.J. Habibi). Jadilah mata air, itu pesan almarhum ayahnya yang selalu diingat Rudy Habibie. Pesan itu yang membawanya terbang kuliah di teknik penerbangan universitas RWTH di kota Aachen demi mewujudkan keinginannya membangun Industri Dirgantara di Indonesia. Di Aachen, Rudy tak hanya harus belajar membuat pesawat tapi juga belajar arti persahabatan. <br/><br/></td></tr> //sinopsis cerita dari film yang akan ditonton
+	  </table>
+	  <input style='width:100px;' type=button value='Pesan Tiket' onclick="window.location.href='kursi2.php';"> //menginputkan kursi yang nanti akan dipilh untuk duduk didalam bioskop
+</body>
+</html>
